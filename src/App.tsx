f89d8974434b0713789988d110930ab28637f041
Adolfo Cruz <adolfoemilianocruzs@gmail.com>
2026-03-09 10:35:52 -0600

    fix footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { AnimatePresence } from 'framer-motion'
+import { FaEnvelope, FaGlobe, FaMapMarkerAlt, FaPhoneAlt, FaWhatsapp } from 'react-icons/fa'
 import { Route, Routes, useLocation } from 'react-router-dom'
-import { SiteHeader } from './components/SiteHeader'
 import logoTecnmHorizontal from './assets/LogoTecNMHorizontal_Blanco.png'
+import { SiteHeader } from './components/SiteHeader'
 import { ConcursoPage } from './pages/ConcursoPage'
 import { HomePage } from './pages/HomePage'
 import { RecursosPage } from './pages/RecursosPage'
@@ -36,8 +37,30 @@ function App() {
 
           <section className="footer-block">
             <h4>Contacto</h4>
-            <p>Correo: capitulo.algoritmia.itm@ejemplo.com</p>
-            <p>Ubicacion: Morelia, Michoacan, Mexico</p>
+            <p className="footer-line">
+              <FaEnvelope aria-hidden="true" />
+              <span>Correo: Aurelio.cr@morelia.tecnm.mx</span>
+            </p>
+            <p className="footer-line">
+              <FaPhoneAlt aria-hidden="true" />
+              <span>Telefono (responsable academico): +52 1 443 160 4078</span>
+            </p>
+            <p className="footer-line">
+              <FaMapMarkerAlt aria-hidden="true" />
+              <span>
+                Direccion: Av. Tecnologico 1500, Colonia Lomas de Santiaguito, C.P. 58120,
+                Morelia, Michoacan.
+              </span>
+            </p>
+            <p className="footer-line">
+              <FaGlobe aria-hidden="true" />
+              <span>
+                Sitio web:{' '}
+                <a href="https://itmorelia.edu.mx" target="_blank" rel="noreferrer">
+                  itmorelia.edu.mx
+                </a>
+              </span>
+            </p>
             <p>Atencion: Lunes a Viernes, 09:00 - 17:00</p>
           </section>
 
@@ -45,7 +68,12 @@ function App() {
             <h4>Enlaces</h4>
             <a href="/concurso">Convocatoria y registro</a>
             <a href="/recursos">Recursos y FAQ</a>
-            <a href="#">Aviso de privacidad</a>
+            <a href="https://whatsapp.com/channel/0029Vb7Iuq1BA1f4srngcy2N" target="_blank" rel="noreferrer">
+              <FaWhatsapp aria-hidden="true" /> Canal de WhatsApp
+            </a>
+            <a href="https://algoritmiaitm.vercel.app" target="_blank" rel="noreferrer">
+              Unete o conoce mas de nosotros
+            </a>
           </section>
         </div>
 
